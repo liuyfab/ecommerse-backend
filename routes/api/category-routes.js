41ea55cf29e91d24e5b22dd348acc83d818532b5
liuyfab <liuyongfeng83@gmail.com>
2021-12-14 22:06:45 -0600

    upload finished models files

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,17 +4,45 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
+  // find all categories 
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
